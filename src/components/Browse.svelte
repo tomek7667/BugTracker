550e4aa7d8677f89ac15eac66b6537aca8e8eb96
tomek7667 <s184571@student.pg.edu.pl>
2022-06-25 13:54:36 +0200

    Frontend browsing programs

--- src/components/Browse.svelte
+++ src/components/Browse.svelte
@@ -1,3 +1,40 @@
+<script>
+    import { onMount } from 'svelte';
+    let programs = [];
+
+    onMount(() => {
+        fetch('http://localhost:3000/api/programs/programs')
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                programs = data;
+            });
+    });
+</script>
+
 <h1>
-    Browse
+    Browsing programs
 </h1>
+
+{#each programs as program (program.id)}
+    <div class="program">
+        <h2>
+            {program.name}
+        </h2>
+        <p>
+            {program.description}
+        </p>
+        <p>
+            <a href="{program.url}">
+                View program
+            </a>
+        </p>
+    </div>
+{/each}
+
+<style>
+    .program {
+        border: 1px solid black;
+        padding: 10px;
+    }
+</style>
