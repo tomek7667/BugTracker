6ec04542f43a8dd59593444d10caaceec82fbd46
tomek7667 <s184571@student.pg.edu.pl>
2022-06-25 04:23:59 +0200

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<script>
+    import Header from './components/Header.svelte'
+    import Login from "./components/Login.svelte";
+    import Register from "./components/Register.svelte";
+    import Home from "./components/Home.svelte";
+    import Profile from "./components/Profile.svelte";
+    import Browse from "./components/Browse.svelte";
+
+    let menu = 1;
+    let isLogged = false;
+</script>
+
+<Header />
+
+<main>
+    {#if menu === 1}
+        <Home />
+    {:else if menu === 2}
+        <Browse />
+    {:else if menu === 3}
+        <Profile />
+    {:else}
+        <h1>
+            Page Not Found
+        </h1>
+    {/if}
+</main>
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+</style>
---
+++ src/components/Header.svelte
@@ -0,0 +1,64 @@
+<script>
+    export let menu = 1;
+    export let isLogged = false;
+</script>
+<img src="img/logo.png" class="logo" on:click={() => {menu = 1}}>
+<div class="wrapper">
+    <ul class="menu">
+        <div class="onLeft">
+            <li><a href="/" on:click|preventDefault={() => (menu = 1)}>Home</a></li> |
+            <li><a href="/" on:click|preventDefault={() => (menu = 2)}>Browse</a></li> |
+            <li><a href="/" on:click|preventDefault={() => (menu = 3)}>Profile</a></li>
+        </div>
+        <div class="onRight">
+        {#if isLogged === true}
+            <li><a href="/" on:click|preventDefault={() => (isLogged = false)}>Logout</a></li>
+        {:else}
+            <li><a href="/" on:click|preventDefault={() => (isLogged = true)}>Register</a></li> |
+            <li><a href="/" on:click|preventDefault={() => (isLogged = true)}>Login</a></li>
+        {/if}
+        </div>
+    </ul>
+</div>
+<style>
+    .logo {
+        height: calc(52px - 20px);
+        float: left;
+        left: 10px;
+        top: 10px;
+        position: relative;
+        cursor: pointer;
+        transition: 0.2s;
+    }
+
+    .logo:hover {
+        opacity: 0.5;
+    }
+
+    .onRight {
+        float: right;
+    }
+
+    .onLeft {
+        float: left;
+    }
+
+    .menu {
+        margin-left: 10px;
+    }
+
+    ul.menu li{
+        display: inline;
+        width: 100%;
+        height: 52px;
+    }
+
+    .wrapper {
+        width: 100%;
+        height: 52px;
+        padding: 15px;
+        background-color: #f5f5f5;
+        border-bottom: 1px solid #e5e5e5;
+    }
+</style>
+
