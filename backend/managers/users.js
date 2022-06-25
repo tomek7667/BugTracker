005634ea47cb41d3ed3805eea2da39859371d3b0
tomek7667 <s184571@student.pg.edu.pl>
2022-06-25 19:12:56 +0200

    Backend authentication and authorization

--- backend/managers/users.js
+++ backend/managers/users.js
@@ -9,56 +9,95 @@ let usersManager = {
     setCon: function (con) {
         this.con = con;
     },
-    login: (user, password) => {
+    login: function (username, password) {
         return new Promise((resolve, reject) => {
-            let login = user.trim();
-            this.con.query("SELECT hash FROM users WHERE login = ?", [login, password], (err, result) => {
-                if (err) return reject({success: false, msg: "Mysql error:" + err.stack});
-                if (result.length === 0) {
-                    return reject({success: false, msg: "No such user found"});
-                } else {
-                    let hashedPassword = result[0].hash;
-                    if (!bcrypt.compareSync(password, hashedPassword)) {
-                        return reject({success: false, msg: "Wrong password"});
+            username = username.trim();
+            if (username.includes('@')) {
+                this.con.query("SELECT hash FROM users WHERE email = ?", [username], (err, result) => {
+                    if (err) return reject({success: false, message: "Mysql error:" + err.stack});
+                    if (result.length === 0) {
+                        return reject({success: false, message: "No such user found"});
                     } else {
-                        return resolve({
-                            success: true, token: jwt.sign(
-                                { loggedAs: login },
+                        let hashedPassword = result[0].hash;
+                        if (!bcrypt.compareSync(password, hashedPassword)) {
+                            return reject({success: false, message: "Wrong password"});
+                        } else {
+                            let token = jwt.sign({
+                                username: username},
                                 privateKey,
-                                { expiresIn: '7d'} )
-                        });
+                                {expiresIn: '7d'}
+                            );
+                            return resolve({success: true, message: "Login successful", token: token});
+                        }
                     }
+                });
+            } else {
+                this.con.query("SELECT hash FROM users WHERE username = ?", [username], (err, result) => {
+                    if (err) return reject({success: false, message: "Error:" + err.sqlMessage});
+                    if (result.length === 0) {
+                        return reject({success: false, message: "No such user found"});
+                    } else {
+                        let hashedPassword = result[0].hash;
+                        if (!bcrypt.compareSync(password, hashedPassword)) {
+                            return reject({success: false, message: "Wrong password"});
+                        } else {
+                            let token = jwt.sign(
+                                { loggedAs: username },
+                                privateKey,
+                                { expiresIn: '7d'}
+                            );
+                            return resolve({success: true, token: token});
+                        }
+                    }
+                });
+            }
+        })
+    },
+    register: function (username, password, email) {
+        return new Promise((resolve, reject) => {
+            username = username.trim();
+            // Check if valid characters
+            if (!/^[a-zA-Z0-9_]*$/.test(username)) return reject({success: false, message: "Your username have wrong characters"});
+            // Check if length is valid
+            if (username.length > 256) return reject({success: false, message: "Your username is too long"});
+            if (username.length < 3) return reject({success: false, message: "Your username is too short"});
+            // Check if the password contains valid characters
+            if (!/^[a-zA-Z0-9!@#$%^&*()_+\-=\[\]{};':"\\|,.?]*$/.test(password)) return reject({success: false, message: "Your password can't have other special characters than [ !@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.? ]"});
+            // Validate an email address
+            if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) return reject({success: false, message: "Your email address is not valid"});
+            if (email.length > 64) return reject({success: false, message: "Your email address is too long"});
+            let hashedPassword = bcrypt.hashSync(password, saltRounds);
+            this.con.query("INSERT INTO users (username, hash, email) VALUES (?, ?, ?)", [username, hashedPassword, email], (err, result) => {
+                if (err) return reject({success: false, message: "Error:" + err.sqlMessage });
+                if (result.length === 0) {
+                    return reject({success: false, message: "Somewthing went wrong"});
+                } else {
+                    let token = jwt.sign(
+                        { loggedAs: username },
+                        privateKey,
+                        { expiresIn: '7d'}
+                    );
+                    return resolve({ success: true, token: token });
                 }
             });
         })
     },
-    verify: (cookie) => {
+    verify: (token) => {
         return new Promise((resolve, reject) => {
-            jwt.verify(cookie, privateKey, (err, decoded) => {
-                    if (err !== null) return reject(false);
-                    else return resolve(decoded.loggedAs);
-                })
+            jwt.verify(token, privateKey, (err, decoded) => {
+                if (err !== null) return reject(false);
+                else return resolve(decoded.loggedAs);
+            })
         })
     },
-    register: (login, password, email) => {
+    updateLastLogged: function (username) {
         return new Promise((resolve, reject) => {
-            let login = login.trim();
-            if (!/^[a-zA-Z0-9_]*$/.test(login)) return reject("Your username have wrong characters");
-            if (login.length > 256) return reject("Your username is too long");
-            if (!/^[a-zA-Z0-9!@#$%^&*()_+\-=\[\]{};':"\\|,.?]*$/.test(password)) reject("Your password can't have other special characters than [ !@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.? ]");
-            // Validate an email address
-            if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) reject("Your email address is not valid");
-            if (email.length > 64) return reject("Your email address is too long");
-            let hashedPassword = bcrypt.hashSync(password, saltRounds);
-            this.con.query("INSERT INTO users (login, bcryptHash) VALUES (?, ?)", [login, hashedPassword], (err, result) => {
-                if (err) return reject("mysql error:" + err.stack);
-                if (result.length === 0) {
-                    return reject({success: false, msg: "Somewthing went wrong"});
+            this.con.query("UPDATE users SET lastLogged = NOW() WHERE username = ?", [username], (err, result) => {
+                if (err) return reject({ message: "Error:" + err.sqlMessage, success: false });
+                if (result.affectedRows === 0) {
+                    return reject({ success: false, message: "Something went wrong" });
                 } else {
-                    let token = jwt.sign({ loggedAs: login }, privateKey, { expiresIn: '7d'});
-                    return resolve({
-                        success: true, token: token
-                    });
+                    return resolve({ success: true });
                 }
             });
         })
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,10 +1,43 @@
 let express = require('express');
 let router = express.Router();
+let usersManager = require('../managers/users');
 
 /* Sanity check */
-router.get('/ping', function(req, res, next) {
+router.get('/ping', (req, res) => {
     res.send('pong!');
 })
 
+router.post('/login',  (req, res) => {
+    // Username can be an e-mail
+    if (req.body.username && req.body.password) {
+        usersManager.login(req.body.username, req.body.password).then((result) => {
+            usersManager.updateLastLogged(req.body.username).then(() => {
+                res.send(result);
+            }).catch((err) => {
+                res.send(err);
+            });
+        }).catch((err) => {
+            res.send(err);
+        });
+    } else {
+        res.send({success: false, message: "Missing username or password"});
+    }
+})
+
+router.post('/register', (req, res) => {
+    if (req.body.username && req.body.password && req.body.email) {
+        usersManager.register(req.body.username, req.body.password, req.body.email).then((result) => {
+            usersManager.updateLastLogged(req.body.username).then(() => {
+                res.send(result);
+            }).catch((err) => {
+                res.send(err);
+            });
+        }).catch((err) => {
+            res.send(err);
+        });
+    } else {
+        res.send({success: false, message: "Missing username, password or email"});
+    }
+})
 
 module.exports = router;
