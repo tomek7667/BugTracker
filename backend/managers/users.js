6a5b0da9c1f9fb7aa070d0bcd5addde9307c3c57
tomek7667 <s184571@student.pg.edu.pl>
2022-06-25 15:39:40 +0200

    Login and verifying bcrypt

--- backend/managers/users.js
+++ backend/managers/users.js
@@ -1,7 +1,55 @@
+const bcrypt = require('bcrypt');
+const fs = require("fs");
+const jwt = require('jsonwebtoken');
+const privateKey = process.env.PRIV_KEY || "Development key";
+const saltRounds = 12;
+
 let usersManager = {
     con: null,
     setCon: function (con) {
         this.con = con;
+    },
+    getUsers: function (callback) {
+        return new Promise((resolve, reject) => {
+            this.con.query('SELECT * FROM users', function (err, result) {
+                if (err) {
+                    return reject(err);
+                } else {
+                    return resolve(result);
+                }
+            });
+        })
+    },
+    login: (user, password) => {
+        return new Promise((resolve, reject) => {
+            let login = user.trim();
+            this.con.query("SELECT hash FROM users WHERE login = ?", [login, password], (err, result) => {
+                if (err) return reject({success: false, msg: "Mysql error:" + err.stack});
+                if (result.length === 0) {
+                    return reject({success: false, msg: "No such user found"});
+                } else {
+                    let hashedPassword = result[0].hash;
+                    if (!bcrypt.compareSync(password, hashedPassword)) {
+                        return reject({success: false, msg: "Wrong password"});
+                    } else {
+                        return resolve({
+                            success: true, token: jwt.sign(
+                                { loggedAs: login },
+                                privateKey,
+                                { expiresIn: '7d'} )
+                        });
+                    }
+                }
+            });
+        })
+    },
+    verify: (cookie) => {
+        return new Promise((resolve, reject) => {
+            jwt.verify(cookie, privateKey, (err, decoded) => {
+                    if (err !== null) return reject(false);
+                    else return resolve(decoded.loggedAs);
+                })
+        })
     }
 }
 
