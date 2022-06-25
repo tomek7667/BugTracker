902d2224bbae439d57bd1b105ef2ba89cdfd16c3
tomek7667 <s184571@student.pg.edu.pl>
2022-06-25 15:56:26 +0200

    Register function, removed testing getUsers

--- backend/managers/users.js
+++ backend/managers/users.js
@@ -9,17 +9,6 @@ let usersManager = {
     setCon: function (con) {
         this.con = con;
     },
-    getUsers: function (callback) {
-        return new Promise((resolve, reject) => {
-            this.con.query('SELECT * FROM users', function (err, result) {
-                if (err) {
-                    return reject(err);
-                } else {
-                    return resolve(result);
-                }
-            });
-        })
-    },
     login: (user, password) => {
         return new Promise((resolve, reject) => {
             let login = user.trim();
@@ -49,6 +38,29 @@ let usersManager = {
                     if (err !== null) return reject(false);
                     else return resolve(decoded.loggedAs);
                 })
+        })
+    },
+    register: (login, password, email) => {
+        return new Promise((resolve, reject) => {
+            let login = login.trim();
+            if (!/^[a-zA-Z0-9_]*$/.test(login)) return reject("Your username have wrong characters");
+            if (login.length > 256) return reject("Your username is too long");
+            if (!/^[a-zA-Z0-9!@#$%^&*()_+\-=\[\]{};':"\\|,.?]*$/.test(password)) reject("Your password can't have other special characters than [ !@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.? ]");
+            // Validate an email address
+            if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) reject("Your email address is not valid");
+            if (email.length > 64) return reject("Your email address is too long");
+            let hashedPassword = bcrypt.hashSync(password, saltRounds);
+            this.con.query("INSERT INTO users (login, bcryptHash) VALUES (?, ?)", [login, hashedPassword], (err, result) => {
+                if (err) return reject("mysql error:" + err.stack);
+                if (result.length === 0) {
+                    return reject({success: false, msg: "Somewthing went wrong"});
+                } else {
+                    let token = jwt.sign({ loggedAs: login }, privateKey, { expiresIn: '7d'});
+                    return resolve({
+                        success: true, token: token
+                    });
+                }
+            });
         })
     }
 }
